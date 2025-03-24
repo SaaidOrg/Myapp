35b095299571ae07ff6140fd3c17c75e737b92fb
saaid111 <saaidmohamed603@gmail.com>
2025-03-24 16:08:50 +0000

    new

--- deploy.sh
+++ deploy.sh
@@ -1,19 +1,20 @@
-sudo apt update && sudo apt install -y nodejs npm
+sudo apt update 
+
+sudo apt install nodejs npm
 
 sudo npm install -g pm2
 
 pm2 stop myapp
 
-cp /home/ubuntu/privatekey.pem SimpleApplication/privatekey.pem
-cp /home/ubuntu/server.crt SimpleApplication/server.crt
+cp /home/ubuntu/privatekey.pem Myapp/privatekey.pem
+cp /home/ubuntu/server.crt Myapp/server.crt
+
 
 cd Myapp/
 
 npm install 
 
 pm2 start .bin/www --name myapp 
-
-
 
 
 
