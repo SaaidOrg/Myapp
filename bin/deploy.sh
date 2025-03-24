b8feb97e526681a9ceb4f3516414db24c7367fc8
saaid111 <saaidmohamed603@gmail.com>
2025-03-24 14:56:23 +0000

    new

---
+++ bin/deploy.sh
@@ -0,0 +1,14 @@
+sudo apt update && sudo apt install nodejs npm
+
+sudo npm install -g pm2
+
+pm2 stop myapp
+
+cd Myapp
+
+npm install 
+
+echo $PRIVATE_KEY > privatekey.pem
+echo $SERVER > server.crt
+
+pm2 start .bin/www --name myapp
